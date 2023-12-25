ef041abb88629de709fffd234b7a3c68eeec1182
theforestvn88 <theforestvn88@gmail.com>
2023-12-25 20:38:36 +0700

    simplify

--- app/helpers/select7/form_helper.rb
+++ app/helpers/select7/form_helper.rb
@@ -1,12 +1,16 @@
 module Select7::FormHelper
     class ActionView::Helpers::FormBuilder
         def select7(name, option_items = [], selected_items = [], suggest_path = nil, **attributes)
+            option_items.map! {|(id, name)| [id, name, name.downcase] }
             attributes.reverse_merge!(css: {}, multiple: true)
+            
             @template.render partial: "select7/form_field", 
                 locals: { scope: @object_name, field: name,  selected_items: selected_items, option_items: option_items, suggest_path: suggest_path, **attributes }
         end
 
         def select7_fields_for(record_name, field, option_items: [], selected_items: [], suggest_path: nil, **attributes)
+            option_items.map! {|(id, name)| [id, name, name.downcase] }
+
             nested_attributes = nested_attributes_association?(record_name)
             association =  nested_attributes ? "#{record_name}_attributes" : record_name
             scope = "#{@object_name}[#{association}]"
--- select7.gemspec
+++ select7.gemspec
@@ -6,8 +6,8 @@ Gem::Specification.new do |spec|
   spec.authors     = ["Lam Phan"]
   spec.email       = ["theforestvn88@gmail.com"]
   spec.homepage    = "https://github.com/theforestvn88/rails-select7.git"
-  spec.summary     = "similar to select2, but no jquery, only for rails7 (+ stimulus.js)"
-  spec.description = "similar to select2, but no jquery, only for rails7 (+ stimulus.js)"
+  spec.summary     = "selector (similar to select2, but no jquery, only use stimulus.js) for rails"
+  spec.description = "selector (similar to select2, but no jquery, only use stimulus.js) for rails"
     spec.license     = "MIT"
   
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the "allowed_push_host"
@@ -22,5 +22,5 @@ Gem::Specification.new do |spec|
     Dir["{app,config,db,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.md"]
   end
 
-  spec.add_dependency "rails", ">= 7.0"
+  spec.add_dependency "rails"#, ">= 7.0"
 end
