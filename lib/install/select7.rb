570f8aa5a30c39bf9e0766884d2c3bb89beb5ce1
theforestvn88 <theforestvn88@gmail.com>
2024-03-22 22:33:57 +0700

    using minified js files

--- lib/install/select7.rb
+++ lib/install/select7.rb
@@ -5,7 +5,7 @@ say "Import Select7 Js"
 
 if IMPORTMAP_PATH.exist?
   append_to_file IMPORTMAP_PATH, %(\npin "lodash.debounce", to: "https://cdn.jsdelivr.net/npm/lodash.debounce@4.0.8/+esm", preload: true\n)
-  append_to_file IMPORTMAP_PATH, %(pin "select7", to: "select7.esm.js", preload: true\n)
+  append_to_file IMPORTMAP_PATH, %(pin "select7", to: "select7.esm.min.js", preload: true\n)
   
   copy_file "#{__dir__}/app/javascript/controllers/select7_esm_controller.js", "app/javascript/controllers/select7_controller.js"
 
@@ -14,15 +14,12 @@ elsif Rails.root.join("package.json").exist?
     run "yarn add lodash.debounce"
     
     insert_into_file APPLICATION_LAYOUT_PATH.to_s, <<~ERB.indent(4), before: /\s*<\/head/
-      \n<%= javascript_include_tag "select7", "data-turbo-track": "reload", rel: :preload, async: true %>
+      \n<%= javascript_include_tag "select7.min.js", "data-turbo-track": "reload", rel: :preload, async: true %>
     ERB
 
     append_to_file Rails.root.join("app/javascript/controllers/index.js"), <<~ERB
       import Select7Controller from "./select7_controller.js"
       application.register("select7", Select7Controller)
-    ERB
-
-    append_to_file Rails.root.join("app/javascript/application.js"), <<~ERB
     ERB
 
     copy_file "#{__dir__}/app/javascript/controllers/select7_controller.js", "app/javascript/controllers/select7_controller.js"
@@ -37,9 +34,7 @@ end
 # ==============
 
 say "Import Select7 Css"
-if (Rails.root.join("app/assets/stylesheets/application.css")).exist?
-  append_to_file "app/assets/stylesheets/application.css", %(\n@import "select7.css"\n)
-elsif APPLICATION_LAYOUT_PATH.exist?
+if APPLICATION_LAYOUT_PATH.exist?
   insert_into_file APPLICATION_LAYOUT_PATH.to_s, <<~ERB.indent(4), before: /^\s*<%= stylesheet_link_tag/
     <%= stylesheet_link_tag "select7", "select7", "data-turbo-track": "reload" %>
   ERB
